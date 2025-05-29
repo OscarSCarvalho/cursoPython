49d81c074270909d882b57793281219255fabe7f
Oscar carvalho <oscar_barueri@hootmail.com>
2025-05-29 20:57:13 -0300

    atualizaçooes

--- funçao_simples.py
+++ funçao_simples.py
@@ -1,5 +1,9 @@
-def  saudacao():
-    print("Ola, bem vindos(a)")
-
+# Função simples em Python
+def  saudacao(nome= 'Oscar'):  
+    # Definição da função com valor padrão 
+    print(f"Ola, {nome})")
+# Primeira chamada da função (sem argumento):  
 saudacao() 
+# Segunda chamada da função (com argumento):
+saudacao("Ana")
           
